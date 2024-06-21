b6d34b742138782202d3625fc6189a0bebc4af60
İbrahimysr <117601752+ibrahimysr@users.noreply.github.com>
2024-06-21 22:00:55 +0300

    Proje dosyası ekle.

---
+++ genetik_algoritma/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace genetik_algoritma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(231, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(231, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 27);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(229, 205);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(134, 27);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(231, 251);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(134, 27);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox7.Location = new System.Drawing.Point(741, 33);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(171, 27);
+            this.textBox7.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Popülasyon Boyutu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Çaprazlama Oranı(%):";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(642, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 36);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Çöz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Mutasyon Oranı(%):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Seçkinlik Adedi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Jenerasyon Sayısı:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(186, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Parametre Değerleri:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(591, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Sonuç Değeri:";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(235)))), ((int)(((byte)(229)))));
+            this.chart1.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Top;
+            this.chart1.BorderlineColor = System.Drawing.Color.IndianRed;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 23);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Chocolate;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(760, 314);
+            this.chart1.TabIndex = 17;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(235)))), ((int)(((byte)(229)))));
+            this.groupBox1.Controls.Add(this.chart1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(87)))), ((int)(((byte)(92)))));
+            this.groupBox1.Location = new System.Drawing.Point(542, 188);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(766, 340);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Yanıksama Grafiği";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(235)))), ((int)(((byte)(229)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1267, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(235)))), ((int)(((byte)(229)))));
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(87)))), ((int)(((byte)(92)))));
+            this.groupBox2.Location = new System.Drawing.Point(43, 25);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1273, 143);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fonksiyon";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(235)))), ((int)(((byte)(229)))));
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(87)))), ((int)(((byte)(92)))));
+            this.groupBox3.Location = new System.Drawing.Point(43, 188);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(444, 340);
+            this.groupBox3.TabIndex = 21;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Değişkenler";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(235)))), ((int)(((byte)(229)))));
+            this.groupBox4.Controls.Add(this.pictureBox2);
+            this.groupBox4.Controls.Add(this.richTextBox1);
+            this.groupBox4.Controls.Add(this.textBox7);
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(87)))), ((int)(((byte)(92)))));
+            this.groupBox4.Location = new System.Drawing.Point(46, 558);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(1279, 137);
+            this.groupBox4.TabIndex = 22;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Sonuçlar";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1053, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(206, 105);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(240, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(201, 96);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(223)))), ((int)(((byte)(187)))));
+            this.ClientSize = new System.Drawing.Size(1434, 707);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ genetik_algoritma/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace genetik_algoritma
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            int populationSize = int.Parse(textBox1.Text); 
+            double crossoverRate = double.Parse(textBox2.Text); 
+            double mutationRate = double.Parse(textBox3.Text); 
+            int elitismCount = int.Parse(textBox4.Text);
+            int generationCount = int.Parse(textBox5.Text); 
+
+            
+            Func<double[], double> fitness = x =>
+            {
+                double sum = 0; 
+                for (int i = 0; i < x.Length - 1; i++)
+                {
+                    // Rosenbrock fonksiyonunun hesaplanması
+                    sum += 100 * Math.Pow(x[i + 1] - x[i], 2) + Math.Pow(x[i] - 1, 2);
+                }
+                return sum; 
+            };
+
+           
+            var ga = new GeneticAlgorithm(populationSize, crossoverRate, mutationRate, elitismCount);
+          
+            var bestSolution = ga.Solve(fitness, generationCount);
+
+            // Sonuçların gösterilmesi
+            richTextBox1.Text = string.Join(" - ", bestSolution); 
+            textBox7.Text = fitness(bestSolution).ToString();
+
+            PlotConvergence(ga.GetConvergenceHistory()); 
+        }
+
+     
+        private void PlotConvergence(List<double> convergenceHistory)
+        {
+          
+            chart1.Series.Clear(); 
+            chart1.Titles.Clear();
+
+           
+            double maxFitnessValue = convergenceHistory.Max(); 
+            chart1.ChartAreas[0].AxisY.Minimum = 0; 
+            chart1.ChartAreas[0].AxisY.Maximum = maxFitnessValue; 
+
+           
+            chart1.ChartAreas[0].AxisY.Interval = maxFitnessValue / 10; 
+            chart1.ChartAreas[0].AxisY.IntervalType = DateTimeIntervalType.Number; 
+
+            double maxIteration = convergenceHistory.Count - 1;
+            chart1.ChartAreas[0].AxisX.Minimum = 0; 
+            chart1.ChartAreas[0].AxisX.Maximum = maxIteration; 
+
+            Series series = new Series(); 
+            series.ChartType = SeriesChartType.Line; 
+            series.BorderWidth = 2; 
+            series.Color = Color.FromArgb(135, 87, 92); 
+
+           
+            for (int i = 0; i < convergenceHistory.Count; i++)
+            {
+                series.Points.AddXY(i, convergenceHistory[i]); 
+            }
+
+            chart1.Series.Add(series);
+
+          
+            chart1.ChartAreas[0].AxisX.Title = "İterasyon"; 
+            chart1.ChartAreas[0].AxisY.Title = "Uygunluk Değeri"; 
+
+           
+            chart1.Invalidate(); 
+        }
+    }
+}
+
+
+
+public class GeneticAlgorithm
+{
+    private int populationSize; 
+    private double crossoverRate; 
+    private double mutationRate; 
+    private int elitismCount;
+    private List<double> convergenceHistory; 
+    
+    private static readonly Random Random = new Random();
+
+ 
+    public GeneticAlgorithm(int populationSize, double crossoverRate, double mutationRate, int elitismCount)
+    {
+        this.populationSize = populationSize; 
+        this.crossoverRate = crossoverRate; 
+        this.mutationRate = mutationRate; 
+        this.elitismCount = elitismCount; 
+        this.convergenceHistory = new List<double>();
+    }
+
+    public double[] Solve(Func<double[], double> fitness, int generationCount)
+    {
+        // Popülasyon oluşturulur
+        var population = new List<double[]>(); 
+        for (int i = 0; i < populationSize; i++)
+        {
+            population.Add(RandomArray(10, -5, 10)); 
+        }
+
+        // Jenerasyonlar boyunca döngü yapılır
+        for (int i = 0; i < generationCount; i++)
+        {
+            // Uygunluk değerleri hesaplanır
+            var fitnessValues = new List<double>();
+            foreach (var chromosome in population)
+            {
+                fitnessValues.Add(fitness(chromosome));
+            }
+            convergenceHistory.Add(fitnessValues.Min()); 
+
+            // Seçilim işlemi yapılır
+            var selectedChromosomes = new List<double[]>(); 
+            for (int j = 0; j < populationSize; j++)
+            {
+                selectedChromosomes.Add(SelectChromosome(population, fitnessValues)); 
+            }
+
+            // Çaprazlama işlemi yapılır
+            var newPopulation = new List<double[]>(); 
+            for (int j = 0; j < populationSize; j++)
+            {
+                if (Random.NextDouble() < crossoverRate)
+                {
+                    var parent1 = selectedChromosomes[j];
+                    var parent2 = selectedChromosomes[Random.Next(populationSize)]; 
+                    newPopulation.Add(Crossover(parent1, parent2)); 
+                }
+                else
+                {
+                    newPopulation.Add(selectedChromosomes[j]); 
+                }
+            }
+
+            // Mutasyon işlemi yapılır
+            foreach (var chromosome in newPopulation)
+            {
+                Mutate(chromosome, mutationRate);
+            }
+
+            // Elitizm işlemi yapılır
+            var elitistChromosomes = new List<double[]>();
+            for (int j = 0; j < elitismCount; j++)
+            {
+
+                elitistChromosomes.Add(population[fitnessValues.IndexOf(fitnessValues.Min())]);
+            }
+
+            // Yeni popülasyon oluşturulur
+            population = new List<double[]>(elitistChromosomes); 
+            population.AddRange(newPopulation); 
+        }
+
+        // En iyi çözüm döndürülür
+        var bestFitness = fitness(population[0]); 
+        var bestChromosome = population[0]; 
+        foreach (var chromosome in population)
+        {
+            var chromosomeFitness = fitness(chromosome); 
+            if (chromosomeFitness < bestFitness)
+            {
+                bestFitness = chromosomeFitness; 
+                bestChromosome = chromosome; 
+            }
+        }
+        return bestChromosome; 
+    }
+
+    // Yakınsama geçmişi getirme metodu
+    public List<double> GetConvergenceHistory()
+    {
+        return convergenceHistory; 
+    }
+
+    // Rastgele dizi oluşturma metodu
+    private double[] RandomArray(int size, double min, double max)
+    {
+        var array = new double[size]; 
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = Random.NextDouble() * (max - min) + min; 
+        }
+        return array; 
+    }
+
+    // Birey seçme metodu turuva metodu
+    private double[] SelectChromosome(List<double[]> population, List<double> fitnessValues)
+    {
+        int index1 = Random.Next(population.Count); 
+        int index2 = Random.Next(population.Count); 
+        while (index1 == index2)
+        {
+            index2 = Random.Next(population.Count); 
+        }
+        return fitnessValues[index1] < fitnessValues[index2] ? population[index1] : population[index2]; 
+    }
+
+    // Çaprazlama metodu tek noktalı çaprazlama 
+    private double[] Crossover(double[] parent1, double[] parent2)
+    {
+        int crossoverPoint = Random.Next(parent1.Length - 1); 
+        var child = new double[parent1.Length]; 
+        for (int i = 0; i < crossoverPoint; i++)
+        {
+            child[i] = parent1[i]; 
+        }
+        for (int i = crossoverPoint; i < parent1.Length; i++)
+        {
+            child[i] = parent2[i];
+        }
+        return child""s; 
+    }
+
+    // Mutasyon metodu
+    private void Mutate(double[] chromosome, double mutationRate)
+    {
+        for (int i = 0; i < chromosome.Length; i++)
+        {
+            if (Random.NextDouble() < mutationRate)
+            {
+                chromosome[i] += Random.NextDouble() * 0.2 - 0.1; 
+                chromosome[i] = Math.Max(chromosome[i], -5); 
+                chromosome[i] = Math.Min(chromosome[i], 10);
+            }
+        }
+    }
+}
